7c6375e08f771d7e7327877da216cef7591cc188
Vilain Thomas <Thomas@MacBook-Pro-de-Thomas.local>
2022-10-25 12:00:48 +0200

    search view

---
+++ src/views/SearchView.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import SearchMovies from "../components/Search";
+
+const SearchMovies = () => {
+    return (
+        <Fragfment>
+            <h1>Search</h1>
+            <SearchMovies />
+        </Fragfment>
+    )
+}
+
+export default SearchMovies
