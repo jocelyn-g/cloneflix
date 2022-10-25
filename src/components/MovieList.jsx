17607858b763c85b8805aff66ff6dc2d4f0274b8
Idriss <idriss24@hotmail.fr>
2022-10-25 11:41:07 +0200

    change MovieList

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,13 +1,52 @@
 import React from "react"
-import { findRenderedDOMComponentWithTag } from "react-dom/test-utils";
 const { Fragment, useState, useEffect } = React
 
 
 
 const MoviesList =({requete}) =>{
-
+const serieId = [
+    {id: 10759, name: 'Action & Adventure'},
+    {id: 16, name: 'Animation'},
+    {id: 35, name: 'Comedy'},
+    {id: 80, name: 'Crime'},
+    {id: 99, name: 'Documentary'},
+    {id: 18, name: 'Drama'},
+    {id: 10751, name: 'Family'},
+    {id: 10762, name: 'Kids'},
+    {id: 9648, name: 'Mystery'},
+    {id: 10763, name: 'News'},
+    {id: 10764, name: 'Reality'},
+    {id: 10765, name: 'Sci-Fi & Fantasy'},
+    {id: 10766, name: 'Soap'},
+    {id: 10767, name: 'Talk'},
+    {id: 10768, name: 'War & Politics'},
+    {id: 37, name: 'Western'}
+]
+const movieId = {
+    28:{id: 28, name: 'Action'},
+    12:{id: 12, name: 'Adventure'},
+    16:{id: 16, name: 'Animation'},
+    35:{id: 35, name: 'Comedy'},
+    80:{id: 80, name: 'Crime'},
+    99:{id: 99, name: 'Documentary'},
+    18:{id: 18, name: 'Drama'},
+    10751:{id: 10751, name: 'Family'},
+    14:{id: 14, name: 'Fantasy'},
+    36:{id: 36, name: 'History'},
+    27:{id: 27, name: 'Horror'},
+    10402:{id: 10402, name: 'Music'},
+    9648:{id: 9648, name: 'Mystery'},
+    10749:{id: 10749, name: 'Romance'},
+    878:{id: 878, name: 'Science Fiction'},
+    10770:{id: 10770, name: 'TV Movie'},
+    53:{id: 53, name: 'Thriller'},
+    10752:{id: 10752, name: 'War'},
+    37:{id: 37, name: 'Western'}
+}
 const [movies, setMovies] = useState([]);
+const [genres, setGenres] = useState([]);
 useEffect(() => {
+
 
     fetch( requete.requete )
     .then( response => response.json() )
@@ -19,10 +58,9 @@ const listStyle = {
     position:'relative',
     overflowX: 'scroll'
 }
-
     return(
                     <Fragment>
-                    <h1>Populaire</h1>
+                    <h1>{}</h1>
                     <div style = {listStyle}>
                         <div style = { { display:'flex' } }>
                         { movies.map( (movie) => <MovieList key={movie.id} movie={movie} /> ) }
@@ -35,7 +73,7 @@ const listStyle = {
 const MovieList = ({movie}) => {
 
     let posterUrl = movie.poster_path ? 'https://image.tmdb.org/t/p/original' + movie.poster_path : 'default-movie-poster.png';
-
+    // console.log(movie)
     const posterStyle = {
         width: '100px',
         height:'150px',
