6d072a36de2ad73253462c5ef4bb160d3183283e
Idriss <idriss24@hotmail.fr>
2022-10-25 13:47:33 +0200

    13h47

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -4,45 +4,6 @@ const { Fragment, useState, useEffect } = React
 
 
 const MoviesList =({requete}) =>{
-const serieId = [
-    {id: 10759, name: 'Action & Adventure'},
-    {id: 16, name: 'Animation'},
-    {id: 35, name: 'Comedy'},
-    {id: 80, name: 'Crime'},
-    {id: 99, name: 'Documentary'},
-    {id: 18, name: 'Drama'},
-    {id: 10751, name: 'Family'},
-    {id: 10762, name: 'Kids'},
-    {id: 9648, name: 'Mystery'},
-    {id: 10763, name: 'News'},
-    {id: 10764, name: 'Reality'},
-    {id: 10765, name: 'Sci-Fi & Fantasy'},
-    {id: 10766, name: 'Soap'},
-    {id: 10767, name: 'Talk'},
-    {id: 10768, name: 'War & Politics'},
-    {id: 37, name: 'Western'}
-]
-const movieId = {
-    28:{id: 28, name: 'Action'},
-    12:{id: 12, name: 'Adventure'},
-    16:{id: 16, name: 'Animation'},
-    35:{id: 35, name: 'Comedy'},
-    80:{id: 80, name: 'Crime'},
-    99:{id: 99, name: 'Documentary'},
-    18:{id: 18, name: 'Drama'},
-    10751:{id: 10751, name: 'Family'},
-    14:{id: 14, name: 'Fantasy'},
-    36:{id: 36, name: 'History'},
-    27:{id: 27, name: 'Horror'},
-    10402:{id: 10402, name: 'Music'},
-    9648:{id: 9648, name: 'Mystery'},
-    10749:{id: 10749, name: 'Romance'},
-    878:{id: 878, name: 'Science Fiction'},
-    10770:{id: 10770, name: 'TV Movie'},
-    53:{id: 53, name: 'Thriller'},
-    10752:{id: 10752, name: 'War'},
-    37:{id: 37, name: 'Western'}
-}
 const [movies, setMovies] = useState([]);
 const [isLoading,setLoad]=useState(true);
 useEffect(() => {
--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -6,7 +6,7 @@ const Home = () => {
         <div className="content">
         <h1>Accueil</h1>
         <MoviesList requete={{ requete:'https://api.themoviedb.org/3/trending/all/day?api_key=f9ac7a805563a418711063c76bd10794&page=1&'}}/>
-        <MoviesList requete={{genre:'28', requete:'https://api.themoviedb.org/3/discover/movie?api_key=f9ac7a805563a418711063c76bd10794&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1&with_genres=28&with_watch_monetization_types=flatrate'}}/>
+        <MoviesList requete={{genre:'Action', requete:'https://api.themoviedb.org/3/discover/movie?api_key=f9ac7a805563a418711063c76bd10794&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1&with_genres=28&with_watch_monetization_types=flatrate'}}/>
         </div>
     )
     
